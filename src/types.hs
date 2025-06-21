d34471805b7662639b01b8a9ca8331df1fd128b1
Daniel S Jeremiah <danielshunom2@gmail.com>
2025-06-21 15:55:38 -0400

    added char parser

--- src/types.hs
+++ src/types.hs
@@ -2,10 +2,26 @@ module Types (JsonValue(..)) where
 
 data JsonValue 
   = JsonNull
-  | JsonBoolean
-  | JsonString
-  | JsonNumber
+  | JsonBoolean Bool
+  | JsonString [Char]
+  | JsonNumber Integer --No float support
   | JsonArray [JsonValue]
   | JsonObject [(String, JsonValue)]
-  | JsonEnum [String]
   deriving (Show, Eq)
+
+-- Ideally we sould want to use a result type here and not `Maybe`
+
+newtype Parser a = Parser{ 
+  runParser :: String -> Maybe (String, a)
+}
+
+charP :: Char -> Parser Char
+charP x = Parser f 
+  where 
+    f (h:tl)
+      | h == x = Just (tl, x)
+      | otherwise = Nothing
+    f [] = Nothing
+
+jsonValue :: Parser JsonValue
+jsonValue = undefined
